a6e607c48004ccea852f90a3ea160df68eeed61f
Josh Cross <crossjoshua6@gmail.com>
2017-12-05 12:00:32 +0000

    Fixed issue where /api/customers or anything else in API would not show up.

---
+++ Dtos/CustomerDto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel.DataAnnotations;
+using System.Web;
+using Vidly.Models;
+
+namespace Vidly.Dtos
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Please enter customer’s name.")]
+        [StringLength(255)]
+        public string Name { get; set; }
+        public bool IsSubscribedToNewsletter { get; set; }
+        public MembershipType MembershipType { get; set; }
+
+        //what we want the form to be displayed as in the browser.
+        [Display(Name = "Membership Type")]
+        public byte MembershipTypeId { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:MM/dd/yyyy}")]
+
+        [Display(Name = "Date of Birth")]
+        [Min18YearsIfAMember]
+        public DateTime? DOB { get; set; }
+    }
+}
