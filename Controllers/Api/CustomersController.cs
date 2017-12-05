dfc98e7ddd7ec819aaca1899e674120344674d8c
Josh Cross <crossjoshua6@gmail.com>
2017-12-05 12:32:56 +0000

    changed the API for customer, so instead of using the domain models, we instead using a DTO (data transfer objects) which makes it harder for hackers.

--- Controllers/Api/CustomersController.cs
+++ Controllers/Api/CustomersController.cs
@@ -5,6 +5,8 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using Vidly.Models;
+using Vidly.Dtos;
+using AutoMapper;
 
 namespace Vidly.Controllers.Api
 {
@@ -18,13 +20,13 @@ namespace Vidly.Controllers.Api
 
 
         // GET /api/customers
-        public IEnumerable<Customer> GetCustomers()
+        public IEnumerable<CustomerDto> GetCustomers()
         {
-            return _context.Customers.ToList();
+            return _context.Customers.ToList().Select(Mapper.Map<Customer, CustomerDto>);
         }
 
         //GET /api/customers/1
-        public Customer GetCustomer(int id)
+        public CustomerDto GetCustomer(int id)
         {
             var customer = _context.Customers.SingleOrDefault(C => C.Id == id);
 
@@ -35,28 +37,32 @@ namespace Vidly.Controllers.Api
                 throw new HttpResponseException(HttpStatusCode.NotFound);
             }
 
-            return customer;
+            return Mapper.Map<Customer, CustomerDto>(customer);
         }
 
         // POST /api/customers
         //only be called if we send a HttpPost request
         [HttpPost]
-        public Customer CreateCustomer(Customer customer)
+        public CustomerDto CreateCustomer(CustomerDto customerDto)
         {
             if (!ModelState.IsValid)
             {
                 throw new HttpResponseException(HttpStatusCode.BadRequest);
             }
 
+            var customer = Mapper.Map<CustomerDto, Customer>(customerDto);
+
             _context.Customers.Add(customer);
             _context.SaveChanges();
 
-            return customer;
+            customerDto.Id = customer.Id;
+
+            return customerDto;
         }
 
         // PUT /api/customers/1
         [HttpPut]
-        public void UpdateCustomer(int id, Customer customer)
+        public void UpdateCustomer(int id, CustomerDto customerDto)
         {
             if (!ModelState.IsValid)
             {
@@ -70,10 +76,11 @@ namespace Vidly.Controllers.Api
                 throw new HttpResponseException(HttpStatusCode.NotFound);
             }
 
-            customerInDb.Name = customer.Name;
-            customerInDb.DOB = customer.DOB;
-            customerInDb.IsSubscribedToNewsletter = customer.IsSubscribedToNewsletter;
-            customer.MembershipTypeId = customer.MembershipTypeId;
+            Mapper.Map<CustomerDto, Customer>(customerDto, customerInDb);
+            customerInDb.Name = customerDto.Name;
+            customerInDb.DOB = customerDto.DOB;
+            customerInDb.IsSubscribedToNewsletter = customerDto.IsSubscribedToNewsletter;
+            customerDto.MembershipTypeId = customerDto.MembershipTypeId;
 
             _context.SaveChanges();
         }
--- Dtos/CustomerDto.cs
+++ Dtos/CustomerDto.cs
@@ -14,15 +14,12 @@ namespace Vidly.Dtos
         [StringLength(255)]
         public string Name { get; set; }
         public bool IsSubscribedToNewsletter { get; set; }
-        public MembershipType MembershipType { get; set; }
 
         //what we want the form to be displayed as in the browser.
-        [Display(Name = "Membership Type")]
         public byte MembershipTypeId { get; set; }
         [DataType(DataType.Date)]
         [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:MM/dd/yyyy}")]
 
-        [Display(Name = "Date of Birth")]
         [Min18YearsIfAMember]
         public DateTime? DOB { get; set; }
     }
